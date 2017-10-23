7b1e42dcbc2fdfd82adbc733c723ea37f5c9b7b7
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-10-23 14:45:08 -0700

    Add code review comments.

--- Test/functionsTest.js
+++ Test/functionsTest.js
@@ -1,6 +1,8 @@
-const mocha = require('mocha');
+const mocha = require('mocha'); // do you ever use the variable 'mocha'?
 const chai = require('chai');
 const expect = chai.expect;
+
+// bds: also lots here that doesn't pass ESLint
 
 const app = require('../functions'); //declares 'app' asLooks for functions outside the test folder
 
@@ -14,6 +16,9 @@ describe('isInRange return true if num is less than 50 and greater than 20.',fun
     expect(app.isInRange(22)).to.be.true;
     }),
 
+// bds: in this case it's not throwing an error, but simply returning a value.
+// bds: See https://www.w3schools.com/jsref/jsref_throw.asp for throwing errors.
+// bds: You will also need to test differently.
   it('isInRange should throw error "NaN" if "num" is not a number.', function(){
     expect(app.isInRange('name')).to.be.NaN
   });
@@ -37,7 +42,9 @@ describe('isTenOrFive', function(){  // describe is from mocha
     expect(app.isTenOrFive(5)).to.be.true; //assertion from chai
   });
 
+// bds: this description is not entirely accurate -- if the number is 10, it isn't 5, and it should return true
   it('isTenOrFive should throw error if num !=== 5', function(){
+    // bds: again, this is not throwing an error.
     expect(app.isTenOrFive(7)).to.be.false;
   });
 
@@ -46,6 +53,7 @@ describe('isTenOrFive', function(){  // describe is from mocha
   });
 
   it('isTenOrFive should throw error if num !=== 10', function(){
+    // bds: see notes above about throwing errors
     expect(app.isTenOrFive(15)).to.be.false;
   });
 
@@ -54,12 +62,15 @@ describe('isTenOrFive', function(){  // describe is from mocha
 
 //addItemToArray - Add item to the end of arr and return the array.
 describe('addItemToArray adds an element to the end of "arr" then returns the array.', function(){
-  const arr = ['coconut', 'peach', 'kiwi']; 
-  it('addItemToArray should return an array', function(){  
+  const arr = ['coconut', 'peach', 'kiwi'];
+  it('addItemToArray should return an array', function(){
+    // bds: this test doesn't match the description 'addItemToArray should return an array'
     expect(app.addItemToArray("junkString",'apple'),'this is not an array').to.be.false;
   });
 
   it('addItemToArray should have more elements after running than when invoked.', function(){
+    // bds: this should use const, not let, since the value doeesn't change.
+    // bds: a better variable name here would be 'initialLength' or 'startLength'
     let objLength = arr.length;  // store length of input array for later comparison.  Originally put this statement in functions.js which was misguided.
     expect(app.addItemToArray(arr,'apple')).to.have.lengthOf(objLength + 1);
   });
@@ -70,17 +81,17 @@ describe('addItemToArray adds an element to the end of "arr" then returns the ar
 1.  [x] replace 'functions' module references with 'app'
 2.  [x] parens are closed before the '.to'
 
-3.  [x] addItemToArray:  
-3a.   [x] for 'it' statements: replace references to 'functions' with target function name (i.e. 'it('addItemToArray...' instead of 'it(functions...').  
+3.  [x] addItemToArray:
+3a.   [x] for 'it' statements: replace references to 'functions' with target function name (i.e. 'it('addItemToArray...' instead of 'it(functions...').
  b.   [x] store the value of arr.length before running the function.
  c.   [x] complete test for 'value of null' see comments in functions.js
  d.   [x] add error message if object type is not an array.
  e.   [x] remove test command invoking function from finished work.
  f.   [x] add new element 'apple' to test array ['coconut', 'peach', 'kiwi']
- 
+
 4.  [x] isInRange:
  a.   [x] for 'expect(' statements reference module import by using 'app.isInRange', rather than 'isInRange'.
- b.   [x] for 'it' statements: replace references to 'functions' with target function names (i.e. 'it('isInRange...' instead of 'it(functions...').   
+ b.   [x] for 'it' statements: replace references to 'functions' with target function names (i.e. 'it('isInRange...' instead of 'it(functions...').
  c.   [x] correct syntax for 'throw error "NaN" if "num" is not a number.'
 
 5.  [x] isTenOrFive:
--- functions.js
+++ functions.js
@@ -1,27 +1,42 @@
-/*These functions were written for problems from LambdaSchool Pre-Work.  Functions 1-5 (isInRange, isPrime, isTenOrFive, addItemToArray, addProperty).  
+/*These functions were written for problems from LambdaSchool Pre-Work.  Functions 1-5 (isInRange, isPrime, isTenOrFive, addItemToArray, addProperty).
 */
+
+// bds: It looks from your checklist like you installed ESLint -- did you do the check
+// bds: referenced in the install instructions to see whether it was working? This file
+// bds: has many formatting issues that do not pass ESLint.
+
 //isInRange
 function isInRange(num) {
   if(typeof num !== 'number') {
+    // bds: this function returns a boolean. NaN is useful when a function returns a number,
+    // bds: and you want to indicate that the input does not result in a use-able number.
+    // bds: In this case, it would be more sensible to return an error.
     return NaN
   }
-  console.log(num < 50 && num > 20); 
+  // bds: console.log should be removed from finished code, unless it's part of spec
+  console.log(num < 50 && num > 20);
   return (num < 50 && num > 20);
- 
+
 }
 
 //isPrime
 function isPrime(num) {
   if(typeof num !== 'number') {
+    // bds: see comment above about returning NaN when the function returns a boolean.
     return NaN;
   }
 
   if (num === 0 || num === 1){
     return false;
   }
+
+  // bds: You only need to loop to num/2 here -- think about why
+  // bds: challenge: try using reduce instead of an explicit for-loop
   for(var i = 2; i < num; i++)
     if(num % i === 0) return false;
 
+  // bds: You've already checked above to see whether or not the number equals 1.
+  // bds: You don't need to check again here.
   return num !== 1;
 };
 
@@ -31,7 +46,7 @@ function isTenOrFive(num) {
 };
 
 //addItemToArray
-/* 
+/*
 Add item to the end of arr and return the array.
 
     Input Example:
@@ -51,8 +66,9 @@ function addItemToArray(arr, item) {
   console.log(arr.constructor, typeof arr); // checks for type of array (suggested by Serafin)
 
     if(arr.constructor !== Array){  // is it's constructor not equal to the array object built-in to JS?
+    // bds: false isn't that helpful as a result. I would return an error here instead.
     return false;
-  } 
+  }
   arr.push(item);
   return(arr);
 };
@@ -64,8 +80,8 @@ module.exports = {isInRange, isPrime, isTenOrFive, addItemToArray}
   [x] install eslint
   [x] remove run commands from functions.js
   [x] remove test statements from functions.js
-  [x] isInRange:  replace the whole conditional (if-then-else) with returning the boolean expression: "return (num < 50 && num > 20)" 
-  [x] isInRange: remove function call 
+  [x] isInRange:  replace the whole conditional (if-then-else) with returning the boolean expression: "return (num < 50 && num > 20)"
+  [x] isInRange: remove function call
   [x] remove all bds comments
   [x] remove all 'note to self' comments
 */
